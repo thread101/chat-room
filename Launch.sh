d3f1a9a39f77a682f6dd8950637c1e0538820413
thread101 <localthread101@gmail.com>
2026-01-25 00:49:24 +0300

    clean exit implementation

--- Launch.sh
+++ Launch.sh
@@ -11,6 +11,15 @@ WHITE='\033[37m'
 RESET='\033[0m'
 BOLD='\033[1m'
 
+cleanup() {
+    printf "${BOLD}${BLUE}\nExiting...\n\n${RESET}"
+    kill -9 $APP_PID $TUNNEL_PID
+    sleep 3
+    exit 0
+}
+
+trap "cleanup" SIGINT
+
 if [ ! -e ./.App-env ] || ! command -v cloudflared >/dev/null 2>&1; then
     [ ! -e ./configure.sh ] && printf "${BOLD}${RED}Missing dependencies, get the 'configure.sh' file on the current directory.\n${RESET}" >&2 && exit 1
     chmod +x ./configure.sh 2>/dev/null
@@ -25,22 +34,24 @@ APP_PID=$!
 sleep 1
 [ $? -ne 0 ] && printf "${BOLD}${RED}Could not start application!!!\n${RESET}" >&2 && exit 1
 
+[ -e ./cloudflared.log ] && rm ./cloudflared.log
 cloudflared tunnel --url http://localhost:8099 --output json --logfile cloudflared.log&
 TUNNEL_PID=$!
 sleep 3
 
 max_retry=5
 for ((i=0; i<$max_retry; i++)); do
+    [ $(wc -l < ./cloudflared.log) -le 5 ] && ((i--))
     url=$(grep -oE "https://[a-z-]{5,}.trycloudflare.com" ./cloudflared.log)
     [ -n "$url" ] && break
     sleep 2
 done
 
 if [ -z "$url" ]; then
-    printf "${BOLD}${RED}Error could start a tunnel!!!\n${RESET}"
-    pkill -9 $APP_PID $TUNNEL_PID
+    printf "\n${BOLD}${RED}Error could start a tunnel!!!\n\n${RESET}"
+    kill -9 $APP_PID $TUNNEL_PID
 else
-    printf "\n\n${BOLD}${GREEN}Tunnel url: $url${RESET}\n\n"
+    printf "\n${BOLD}${GREEN}Tunnel url: $url${RESET}\n\n"
 fi
 
 wait
